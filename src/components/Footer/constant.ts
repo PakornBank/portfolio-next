e5162a38b213b09c87e30f8a3e8a82ef4d830de3
Pakorn Phongkhunakorn <pakorn22120@gmail.com>
2025-01-22 02:04:18 +0700

    fix: Update social links in Footer component

--- src/components/Footer/constant.ts
+++ src/components/Footer/constant.ts
@@ -2,7 +2,11 @@ import { Github, Linkedin, Mail } from "lucide-react";
 import { SocialLink } from "./type";
 
 export const SOCIAL_LINKS: SocialLink[] = [
-	{ platform: "GitHub", url: "https://github.com", icon: Github },
-	{ platform: "LinkedIn", url: "https://linkedin.com", icon: Linkedin },
-	{ platform: "Email", url: "mailto:your@email.com", icon: Mail },
+	{ platform: "GitHub", url: "https://github.com/PakornBank/", icon: Github },
+	{
+		platform: "LinkedIn",
+		url: "https://www.linkedin.com/in/pakorn-bank",
+		icon: Linkedin,
+	},
+	{ platform: "Email", url: "mailto:pakorn22120@gmail.com", icon: Mail },
 ];
